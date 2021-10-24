078935b8bab366943152a2602b3d7d2696b09161
John G <gkahiu@gmail.com>
2021-10-24 12:24:54 +0300

    Add plugin metadata file

---
+++ flts_service/__init__.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : FLTSServerPlugin
+Description          : Plugin for adding Qr Code and barcode layout in a print
+                       layout
+Date                 : 24-10-2021
+copyright            : (C) 2021 by John Gitau
+                       See the accompanying file CONTRIBUTORS.txt in the root
+email                : gkahiu@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from .plugin import QRBarCodePluginLoader
+
+
+# noinspection PyPep8Naming
+def classFactory(iface):  # pylint: disable=invalid-name
+    """Load the plugin loader class
+
+    :param iface: A QGIS interface instance.
+    :type iface: QgsInterface
+    """
+    return QRBarCodePluginLoader(iface)
