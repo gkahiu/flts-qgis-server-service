bcf14cea66b9977649c0f7dca69394bcdaf4f982
John G <gkahiu@gmail.com>
2021-11-03 22:27:03 +0300

    Add basic custom renderer for PDF documents

--- flts_service/core/handlers.py
+++ flts_service/core/handlers.py
@@ -19,15 +19,24 @@ email                : gkahiu@gmail.com
 """
 from dataclasses import dataclass
 from collections import OrderedDict
+from typing import Dict
+from zipfile import ZipFile
+from abc import (
+    ABC,
+    abstractmethod,
+    abstractproperty
+)
 
 from qgis.PyQt.QtCore import (
-    QByteArray,
-    QFile
+    QDateTime,
+    QDir,
+    QFile,
+    QTemporaryFile
 )
 from qgis.core import (
     Qgis,
-    QgsMessageLog,
-    QgsProject
+    QgsLayoutExporter,
+    QgsMessageLog
 )
 from qgis.server import (
     QgsServerInterface,
@@ -35,6 +44,10 @@ from qgis.server import (
 )
 
 from flts_service.core.exception import FltsServiceException
+from flts_service.core.renderer import (
+    FltsRenderer,
+    RendererContext
+)
 from flts_service.core.request import FltsServerRequest
 from flts_service.core.utils import write_json_response
 
@@ -81,17 +94,21 @@ def create_project_meta():
     return projects
 
 
-class BaseRequestHandler:
+class BaseRequestHandler(ABC):
     """Provides common functionality across all request handlers.
     """
-    # Should match request name sent by server
+    # Should match request name handed over by server
     REQUEST_ID = ''
+
+    # Used to exclude handler from being registered
+    EXCLUDE_REGISTER_ID = 'BACKEND'
 
     # Container for registering sub-classes
     handlers = []
 
-    def __init__(self, server_iface):
-        self._iface = server_iface
+    def __init__(self, iface: QgsServerInterface, service: 'FltsService'):
+        self._iface = iface
+        self._service = service
         self._projects = create_project_meta()
 
     def __init_subclass__(cls, **kwargs):
@@ -105,8 +122,8 @@ class BaseRequestHandler:
                 500,
                 'Internal server error'
             )
-
-        BaseRequestHandler.handlers.append(cls)
+        if cls.REQUEST_ID != BaseRequestHandler.EXCLUDE_REGISTER_ID:
+            BaseRequestHandler.handlers.append(cls)
 
     @property
     def server_iface(self) -> QgsServerInterface:
@@ -116,6 +133,12 @@ class BaseRequestHandler:
         :rtype: QgsServerInterface
         """
         return self._iface
+
+    @property
+    def service(self) -> 'FltsService':
+        """Returns a reference to the FltsService.
+        """
+        return self._service
 
     @classmethod
     def log_error(cls, msg: str):
@@ -127,32 +150,8 @@ class BaseRequestHandler:
         # Log information message
         QgsMessageLog.logMessage(msg, level=Qgis.Info)
 
-    def project_by_id(self, temp_id: str) -> ProjectInfo:
-        """
-        Retrieves a ProjectInfo object, containing a QGS project object,
-        based on specified ID registered in the our collection.
-        """
-        return self._projects.get(temp_id, None)
-
-    def send_docs(
-            self,
-            docs: list,
-            response: QgsServerResponse
-    ):
-        """Write files to response using the given content type. If
-        only one file in the list then the content type will be pdf.
-        `docs` contains a list of QByteArray objects.
-        """
-        content_type = 'application/pdf'
-        if len(docs) > 1:
-            content_type = 'application/zip'
-
-    def exec_request(
-            self,
-            request: FltsServerRequest,
-            response: 'QgsServerResponse',
-            service: 'QgsService'
-    ):
+    @abstractmethod
+    def exec_request(self, request: FltsServerRequest, response: 'QgsServerResponse'):
         """
         Execute request sent by our custom service. Base implementation does
         nothing, to be implemented by sub-classes.
@@ -160,40 +159,75 @@ class BaseRequestHandler:
         pass
 
 
-def render_document(
-        project_path: str,
-        field_values: dict,
-        request: FltsServerRequest,
-        iface: QgsServerInterface,
-        layout:str = 'Main'
-) -> QByteArray:
-    """Generate single document based on project file and values for
-    corresponding fields/items in the layout.
+class AbstractDocumentRequestHandler(BaseRequestHandler):
+    """For use by handlers that require document functionality.
     """
-    # Check if the project file exists
-    if not QFile.exists(project_path):
-        raise FltsServiceException(
-            404,
-            'Layout template not found'
+    REQUEST_ID = BaseRequestHandler.EXCLUDE_REGISTER_ID
+
+    @abstractproperty
+    def document_prefix(self) -> str:
+        """Text to append before document file name. Abstract implementation
+        returns an empty string.
+        """
+        return ''
+
+    def create_archive(
+            self,
+            docs: Dict[str, QTemporaryFile],
+            temp_dir: str
+    ) -> str:
+        # Zip the documents and name archive using current date/time
+        archive_name = '{0}/{1}_archive_{2}.zip'.format(
+            temp_dir,
+            self.document_prefix,
+            QDateTime.currentDateTime().toString('yyyyMMddhhmmss')
         )
+        with ZipFile(archive_name, 'w') as archive:
+            for file_name, temp_file in docs.items():
+                archive.write(temp_file, '{0}.pdf'.format(file_name))
 
-    project = QgsProject.instance().read(project_path)
+        return archive_name
 
-    # Inject custom parameters into WMS parameters object
-    request.setParameter('MAP', project_path)
-    request.setParameter('TEMPLATE', layout)
-    for k, v in field_values.items():
-        request.setParameter(k, v)
+    def project_by_id(self, temp_id: str) -> ProjectInfo:
+        """
+        Retrieves a ProjectInfo object, containing a QGS project object,
+        based on specified ID registered in the our collection.
+        """
+        return self._projects.get(temp_id, None)
 
-    wms_req = QgsWmsRequest(request)
-    wms_params = wms_req.wmsParameters()
+    def send_documents(
+            self,
+            docs: Dict[str, QTemporaryFile],
+            response: QgsServerResponse,
+            temp_dir: str
+    ):
+        # Package documents and send to the client
+        num_docs = len(docs)
+        if num_docs == 0:
+            data = {'status': 'success', 'message': 'No documents processed'}
+            write_json_response(data, response, 200)
 
-    wms_ctx = QgsWmsRenderContext(project, iface)
-    wms_ctx.setParameters(wms_params)
+            return
 
-    renderer = QgsRenderer(wms_ctx)
+        elif num_docs == 1:
+            # Get first/only document in the collection
+            keys = docs.keys()
+            file_name = keys[0]
+            doc = docs[file_name]
+            response.setHeader('Content-Type', 'application/pdf')
+            response.write(doc.readAll())
 
-    return renderer.getPrint()
+        else:
+            # Create archive
+            archive_path = self.create_archive(docs, temp_dir)
+            file = QFile(archive_path)
+            if not file.exists():
+                FltsServiceException(
+                    500,
+                    'Could not create document archive.'
+                )
+            response.setHeader('Content-Type', 'application/zip')
+            response.write(file.readAll())
 
 
 class CapabilitiesRequestHandler(BaseRequestHandler):
@@ -204,32 +238,46 @@ class CapabilitiesRequestHandler(BaseRequestHandler):
     def exec_request(
             self,
             request: FltsServerRequest,
-            response: 'QgsServerResponse',
-            service: 'QgsService'
+            response: 'QgsServerResponse'
     ):
         # Sample capabilities
         info = {
-            'Name': service.name(),
-            'Version': service.version(),
-            'Description': 'Demo service for generating starter title...',
+            'Name': self.service.name(),
+            'Version': self.service.version(),
+            'Description': 'Demo service that currently supports starter title...',
             'Contact Person': 'John Gitau',
             'Contact Organization': 'xxxx yyyy'
         }
         write_json_response(info, response, 200)
 
 
-class StarterRequestHandler(BaseRequestHandler):
+class StarterRequestHandler(AbstractDocumentRequestHandler):
     """Demo handler for simulating generation of starter certificates.
     """
     REQUEST_ID = 'GetStarterCert'
 
+    @property
+    def document_prefix(self) -> str:
+        """Appended to document or archive file names.
+        """
+        return 'Starter_Title'
+
+    def renderer_context(self, info: ProjectInfo) -> RendererContext:
+        """Create the context for using in the renderer.
+        """
+        ctx = RendererContext(info.path, self.server_iface)
+        ctx.proj_title = 'Starter Title'
+        ctx.proj_abstract = 'Document produced by FLTS starter title module'
+        ctx.proj_author = 'CB-FLTS Document Author'
+        ctx.layout_id = 'Main'
+
+        return ctx
+
     def exec_request(
             self,
             request: FltsServerRequest,
-            response: 'QgsServerResponse',
-            service: 'QgsService'
+            response: 'QgsServerResponse'
     ):
-        # Generate starter title certificates
         template_id = request.template_id
         if not template_id:
             raise FltsServiceException(
@@ -250,18 +298,28 @@ class StarterRequestHandler(BaseRequestHandler):
         if count_enum == FltsServerRequest.NOT_DEFINED:
             count_enum = FltsServerRequest.FIRST
 
-        vars = proj_info.variable_map
-        i = 0
-        docs = []
-        for k, field_values in vars.items():
-            ba = render_document(
-                proj_info.path,
-                field_values,
-                request,
-                self.server_iface
-            )
-            docs.append(ba)
-            # Fetch only the first one if enum is set to FIRST
-            if count_enum == FltsServerRequest.FIRST:
-                break
+        # Temp directory for documents
+        doc_dir = QDir.tempPath()
+        doc_files = {}
+        for k, field_values in proj_info.variable_map.items():
+            # Get context
+            renderer_ctx = self.renderer_context(proj_info)
+            renderer_ctx.document_dir = doc_dir
+            renderer_ctx.item_values = field_values
+            renderer = FltsRenderer(renderer_ctx)
+            res, doc = renderer.get_document()
+            if res == QgsLayoutExporter.Success:
+                doc_files[k] = doc
+                # Fetch only the first one if enum is set to FIRST
+                if count_enum == FltsServerRequest.FIRST:
+                    break
+            else:
+                self.log_error(
+                    'Error {0} occurred while exporting layout to PDF'.format(
+                        str(res)
+                    )
+                )
+
+        response.clear()
+        self.send_documents(doc_files, response, doc_dir)
 
---
+++ flts_service/core/renderer.py
@@ -0,0 +1,220 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+Name                 : Renderer
+Description          : Renderer for FLTS requests
+Date                 : 02-11-2021
+copyright            : (C) 2021 by John Gitau
+email                : gkahiu@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 3 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+from typing import Union
+
+from qgis.PyQt.QtCore import (
+    QDateTime,
+    QDir,
+    QFile,
+    QTemporaryFile
+)
+from qgis.core import (
+    Qgis,
+    QgsLayoutExporter,
+    QgsLayoutItemLabel,
+    QgsMessageLog,
+    QgsProject
+)
+
+from flts_service.core.exception import FltsServiceException
+
+
+class RendererContext:
+    """Provides context settings for the renderer.
+    """
+    # Document file naming convention - use record_id or auto-generate (UUID)
+    REC_ID, AUTO_GEN = range(0, 2)
+
+    def __init__(
+            self,
+            project: Union[QgsProject, str],
+            server_iface: 'QgsServerInterface'
+    ):
+        self._project = project
+        if isinstance(self._project, str):
+            self._project = RendererContext.project_from_path(self._project)
+
+        self._server_iface = server_iface
+        self._naming_conv = RendererContext.REC_ID
+
+        # Assume there will be a default template named 'Main'
+        self.layout_id = 'Main'
+        self._item_values = {}
+
+        # Some project properties that will be cascaded to the pdf file. See
+        # QgsProjectMetadata for more options.
+        self.proj_title = ''
+        self.proj_author = 'FLTS Main Author'
+        self.proj_abstract = ''
+
+        # Update project metadata
+        self._update_project_metadata()
+
+        self.document_dir = ''
+
+    @property
+    def project(self) -> QgsProject:
+        """Returns QGIS project object.
+        """
+        return self._project
+
+    def _update_project_metadata(self):
+        # Update based on context properties
+        md = self._project.metadata()
+        md.setTitle(self.proj_title)
+        md.setAuthor(self.proj_author)
+        md.setCreationDateTime(QDateTime.currentDateTime())
+        md.setAbstract(self.proj_abstract)
+        self._project.setMetadata(md)
+
+    @project.setter
+    def project(self, project_path: str):
+        """Set project from path.
+        """
+        self._project = RendererContext.project_from_path(project_path)
+        self._update_project_metadata()
+
+    @staticmethod
+    def project_from_path(self, path: str) -> QgsProject:
+        # Project from the path and set project properties which will be
+        # cascaded to the pdf document.
+        if not QFile.exists(path):
+            raise FltsServiceException(
+                404,
+                'Project file \'{0}\' not found.'.format(path)
+            )
+
+        return QgsProject.instance().read(path)
+
+    @property
+    def naming(self) -> int:
+        """Naming convention for PDF document. Default is to use record_id.
+        """
+        return self._naming_conv
+
+    @naming.setter
+    def naming(self, conv: int):
+        """Set document naming convention.
+        """
+        self._naming_conv = conv
+
+    @property
+    def item_values(self) -> dict:
+        """Return dictionary containing item values with the key
+        corresponding to the item id (in the layout) and value corresponding
+        to the value (e.g. text for a label, layer for a map, file path for
+        picture etc.
+        """
+        return self._item_values
+
+    @item_values.setter
+    def item_values(self, value_map: dict):
+        """Set the item values collection. It overrides the previous
+        collection.
+        """
+        self._item_values = value_map
+
+    def append_item_value(self, item_id: str, value: object):
+        """Specify value for a layout item with the given id. Previous value
+        for the item will be overridden.
+        """
+        self._item_values[item_id] = value
+
+
+class FltsRenderer:
+    """Renders requests to the FLTS service.
+    """
+
+    def __init__(self, ctx: RendererContext):
+        self._ctx = ctx
+
+    @property
+    def context(self) -> RendererContext:
+        """Returns RendererContext used by this class.
+        """
+        return self._ctx
+
+    def _get_layout(self):
+        template_name = self._ctx.layout_id
+        if not template_name:
+            raise FltsServiceException(
+                500,
+                'Template name not specified.'
+            )
+        # Check template
+        lyt_mgr = self._ctx.project.layoutManager()
+        doc_layout = lyt_mgr.layoutByName(template_name)
+        if not doc_layout:
+            raise FltsServiceException(
+                404,
+                '\'{0}\' layout not found in the QGIS project file.'.format(
+                    template_name
+                )
+            )
+        # Layout should have at least one page
+        if doc_layout.pageCollection().pageCount() < 1:
+            raise FltsServiceException(
+                500,
+                'No pages in layout template.'.format(
+                    template_name
+                )
+            )
+
+        return doc_layout
+
+    def _configure_layout(self, layout):
+        """Set values for items in the layout. Currently only supports
+        labels but should also incorporate maps, picture, legend items etc.
+        """
+        for item_id, value in self._ctx.item_values.items():
+            item = layout.itemById(item_id)
+            # Labels
+            if isinstance(item, QgsLayoutItemLabel):
+                # Handle non string data types. Need to incorporate checks
+                # for complex types.
+                if not isinstance(value, str):
+                    value = str(value)
+                item.setText(value)
+
+    def get_document(self) -> tuple(QgsLayoutExporter.ExportResult, QTemporaryFile):
+        """Generate document and return tuple containing export result
+        (success, failure etc.) and temporary file containing the document
+        if the result was success.
+        """
+        layout = self._get_layout()
+        self._configure_layout(layout)
+
+        # Write to temp file
+        doc_dir = self._ctx.document_dir
+        if not doc_dir:
+            doc_dir = QDir.tempPath()
+        temp_file = QTemporaryFile('{0}/XXXXXX.pdf'.format(doc_dir))
+        if not temp_file.open():
+            raise FltsServiceException(
+                500,
+                'Could not open temporary file to write document.'
+            )
+
+        # Write document to temp file
+        settings = QgsLayoutExporter.PdfExportSettings()
+        exporter = QgsLayoutExporter(layout)
+        result = exporter.exportToPdf(temp_file.fileName(), settings)
+
+        return result, temp_file
